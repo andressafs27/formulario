8fdbc348be1ab4b31b6323101284b6f59ed5ef33
Andressa <andreessafariass@gmail.com>
2023-11-07 15:39:13 -0300

    feats: alterações

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ var listaUsuarios = [];
 function processarCadastroUsuario(requisicao, resposta) {
     const usuario = {
         nome: requisicao.query.nome,
-        // Adicione os campos sobrenome e email se necessário
+        sobrenome: requisicao.query.sobrenome,
+        email: requisicao.query.email,
+        senha: requisicao.query.senha,
+        endereco: requisicao.query.endereco
     }
 
     listaUsuarios.push(usuario);
@@ -21,36 +24,54 @@ function processarCadastroUsuario(requisicao, resposta) {
             <body>
                 <h1>Usuários Cadastrados</h1>
                 <table class="table table-hover table-dark">
-                    <!-- Tabela de usuários cadastrados aqui -->
-                </table>
-                <a href="/" class="badge badge-warning">Voltar ao Menu anterior</a>
-                <a href="/cadastra.html" class="badge badge-warning">Continuar Cadastrando</a>
-            </body>
-        </html>`;
+                    <!-- Tabela de usuários cadastrados -->
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Sobrenome</th>
+                            <th>E-mail</th>
+                            <th>Endereço</th>
+                        </tr>
+                    </thead>
+                    <tbody>`;
+    
+                    conteudoResposta += `
 
-    resposta.send(conteudoResposta);
+                </table>
+                    <a href="/" class="badge badge-warning">Voltar ao Menu anterior</a>
+                    <a href="/cadastra.html" class="badge badge-warning">Continuar Cadastrando</a>
+            </body>
+            <script></script>
+        </html>`;
+    
+            resposta.send(conteudoResposta);
 }
 
 const app = express();
 
 app.use(express.static('./paginas'));
 
-app.get('/', (requisicao, resposta) => {
+app.get('/cadastra', (requisicao, resposta) => {
     resposta.end(
         `
         <!DOCTYPE html>
         <html>
             <head>
-                <!-- Restante do cabeçalho -->
+                <meta charset='utf-8'>
+                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                <title>Formulário</title>
+                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                <script src='main.js'></script>
             </head>
             <body>
                 <h1>Menu</h1>
                 <ul>
-                    <li><a href="/cadastra.html">Cadastrar Usuário</a></li>
-                </ul>
+                    <li><a href="/cadastra.html>Cadastrar Usuário</a></li>
+                <ul>
             </body>
         </html>
-        `
+    `
     )
 });
 
